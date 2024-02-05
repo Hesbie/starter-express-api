03d9dd2b5de9c30a4f04afd4530084b6ee6c5faa
Hesbie <hesbonmakori1996@gmail.com>
2024-02-05 15:13:42 +0300

    Undefined token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -17,7 +17,7 @@ function authenticateToken(req, res, next) {
 }
 
 function generateAccessToken(phoneNumber) {
-  return jwt.sign({ data: phoneNumber }, `${process.env.JWT_SECRET}`, {
+  return jwt.sign({ data: phoneNumber }, ""+ process.env.JWT_SECRETKey, {
     expiresIn: process.env.JWT_LIFETIME,
   });
 }
